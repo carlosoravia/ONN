5fd703067f702e508c3a3dee7a803b76566f1b6e
Softcontrol <carlo.soravia99@gmail.com>
2025-06-10 11:25:27 +0200

    messa in pari

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,11 +10,17 @@ class AdminController extends Controller
 {
     public function index(){
         $audits = AuditLog::all();
+        if (empty($audits)) {
+            return view('admin.index', ['audits' => []]);
+        }
         return view('admin.index', compact('audits'));
     }
 
     public function editUsers(){
         $users = User::all();
+        if (empty($users)) {
+            return view('admin.edit-users', ['users' => []]);
+        }
         return view('admin.edit-users', compact('users'));
     }
 }
