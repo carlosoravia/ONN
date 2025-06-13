82830eac080fa56a5242f6f1caa27907178fd855
Softcontrol <carlo.soravia99@gmail.com>
2025-06-13 14:17:30 +0200

    inserito comando riavvio automatico

---
+++ app/Console/Commands/StartReady.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class StartReady extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:start-ready';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Pulizia cache e avvio scheduler Laravel';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->callSilent('config:clear');
+        $this->callSilent('route:clear');
+        $this->callSilent('view:clear');
+        $this->callSilent('cache:clear');
+
+        $this->call('config:cache');
+        $this->call('route:cache');
+        $this->call('view:cache');
+
+        $this->info('✅ Laravel ottimizzato. Avvio scheduler...');
+        $this->call('schedule:work');
+    }
+}
