b2c3e6b37658862ed08ea3462b824ae35b6ccc7e
Carlo Soravia <carlo.soravia99@gmail.com>
2025-06-23 10:16:10 +0200

    Add Livewire assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 import './messageBtn';
 import Alpine from 'alpinejs';
+import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+Livewire.start();
