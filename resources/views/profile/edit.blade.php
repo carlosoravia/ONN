c48df6388fcb9224072e531ba01a4bbea2dd1822
Softcontrol <carlo.soravia99@gmail.com>
2025-06-30 11:30:18 +0200

    miglioramenti grafici

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="w-fit mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+    <div class="py-12 h-screen">
+        <div class="w-[40vw] h-fit m-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-gray2-900 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
